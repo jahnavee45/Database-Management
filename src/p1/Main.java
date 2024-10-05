0c18e8888663ace995b73813503ece6f5a7727bb
jahnavee45 <roysimran1545@gmail.com>
2024-10-05 12:51:12 +0530

    first commit

---
+++ src/p1/Main.java
@@ -0,0 +1,285 @@
+package p1;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.Statement;
+import java.util.Scanner;
+
+public class Main {
+
+	public static void main(String[] args) {
+
+		// creating variable names same as given in database table column names
+		String name;
+		String email;
+		String mobile;
+
+		// taking user input to perform an SQL operation
+		System.out.println("PERFORM ANY GIVEN SQL OPERATION" + "\n1. Check sql connection" + "\n2. Insert a new member"
+				+ "\n3. Delete a member" + "\n4. Update member details" + "\n5. Retrieve the table data");
+		System.out.print("Choose an option: ");
+
+		Scanner scn = new Scanner(System.in);
+		int option = scn.nextInt();
+		scn.nextLine();
+
+		while (option != 6) {
+			switch (option) {
+
+			case 1: {
+
+				// calling method to check SQL connection
+				option = checkSQLConnection();
+				break;
+			}
+			case 2: {
+
+				System.out.print("\nEnter name: ");
+				name = scn.nextLine();
+				System.out.print("Enter email id: ");
+				email = scn.nextLine();
+				System.out.print("Enter mobile: ");
+				mobile = scn.nextLine();
+
+				// calling method to perform insert operation
+				option = insertQuery(name, email, mobile);
+				break;
+			}
+			case 3: {
+
+				System.out.println("\nEnter email id: ");
+				email = scn.nextLine();
+
+				// calling method to perform delete operation
+				option = deleteQuery(email);
+				break;
+			}
+			case 4: {
+
+				System.out
+						.println("\nWhat do you want to update? " + "\n1. Name " + "\n2. Email" + "\n3. Mobile number");
+				System.out.print("Choose an option: ");
+				int option2 = scn.nextInt();
+				scn.nextLine();
+
+				option = updateQuery(option2);
+				break;
+			}
+			case 5: {
+
+				// calling method to display the table data
+				option = retrieveQuery();
+				break;
+			}
+			}
+		}
+
+		System.out.print("\n-----------SESSION TERMINATED----------");
+		scn.close();
+
+	}
+
+	// CASE 5
+	private static int retrieveQuery() {
+
+		try {
+			// connect to database
+			Connection con = DriverManager.getConnection("jdbc:mysql://127.0.0.1:3306/database_name", "usename", "password");
+
+			// execute SQL query
+			Statement stm = con.createStatement();
+			ResultSet result = stm.executeQuery("select * from registration");
+
+			// display table in a proper table format
+			System.out.println("\nRegistration Table Data");
+			System.out.printf("%-15s %-20s %-15s%n", "Name", "Email", "Mobile");
+			System.out.println("---------------------------------------------------");
+			while (result.next()) {
+				System.out.printf("%-15s %-20s %-15s%n", result.getString(1), result.getString(2), result.getString(3));
+			}
+
+			// close database connection
+			con.close();
+
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+
+		// check if user wants to continue or not
+		Scanner scn = new Scanner(System.in);
+		System.out.println("\nDo you want to continue? yes/no");
+		String flag = scn.nextLine();
+
+		if (flag.equals("yes")) {
+			System.out.print("Choose an option again: ");
+			int option = scn.nextInt();
+			return option;
+		}
+		return 6;
+
+	}
+
+	// CASE 4
+	private static int updateQuery(int option) {
+
+		Scanner scn = new Scanner(System.in);
+
+		try {
+
+			String name;
+			String email;
+			String mobile;
+
+			// connect to database
+			Connection con = DriverManager.getConnection("jdbc:mysql://127.0.0.1:3306/database_name", "usename", "password");
+
+			// execute SQL query
+			Statement stm = con.createStatement();
+
+			switch (option) {
+			case 1: {
+				System.out.println("Enter email id of the member: ");
+				email = scn.nextLine();
+				System.out.println("Enter modified name: ");
+				name = scn.nextLine();
+				stm.executeUpdate("Update registration set name = '" + name + "' where email = '" + email + "'");
+				break;
+			}
+			case 2: {
+				System.out.println("Enter name of the member: ");
+				name = scn.nextLine();
+				System.out.println("Enter modified email id: ");
+				email = scn.nextLine();
+				stm.executeUpdate("Update registration set email = '" + email + "' where name = '" + name + "'");
+				break;
+			}
+			case 3: {
+				System.out.println("Enter email id: ");
+				email = scn.nextLine();
+				System.out.println("Enter new mobile number: ");
+				mobile = scn.nextLine();
+				stm.executeUpdate("Update registration set mobile = '" + mobile + "' where email = '" + email + "'");
+				break;
+			}
+			}
+			System.out.println("Updation complete!");
+
+			// close database connection
+			con.close();
+
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+
+		// check if user wants to continue or not
+		System.out.println("\nDo you want to continue? yes/no");
+		String flag = scn.nextLine();
+
+		if (flag.equals("yes")) {
+			System.out.print("Choose an option again: ");
+			int option1 = scn.nextInt();
+			return option1;
+		}
+		return 6;
+
+	}
+
+	// CASE 3
+	private static int deleteQuery(String email) {
+
+		try {
+			// connect to database
+			Connection con = DriverManager.getConnection("jdbc:mysql://127.0.0.1:3306/database_name", "usename", "password");
+
+			// execute SQL query
+			Statement stm = con.createStatement();
+			stm.executeUpdate("delete from registration where email = '" + email + "' ");
+			System.out.println("Deletion complete!");
+
+			// close database connection
+			con.close();
+
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+
+		// check if user wants to continue or not
+		Scanner scn = new Scanner(System.in);
+		System.out.println("\nDo you want to continue? yes/no");
+		String flag = scn.nextLine();
+
+		if (flag.equals("yes")) {
+			System.out.print("Choose an option again: ");
+			int option = scn.nextInt();
+			return option;
+		}
+		return 6;
+
+	}
+
+	// CASE 2
+	private static int insertQuery(String name, String email, String mobile) {
+
+		try {
+			// connect to database
+			Connection con = DriverManager.getConnection("jdbc:mysql://127.0.0.1:3306/database_name", "usename", "password");
+
+			// execute SQL query
+			Statement stm = con.createStatement();
+			stm.executeUpdate("insert into registration values('" + name + "', '" + email + "', '" + mobile + "')");
+			System.out.println("Insertion complete!");
+
+			// close database connection
+			con.close();
+
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+
+		// check if user wants to continue or not
+		Scanner scn = new Scanner(System.in);
+		System.out.println("\nDo you want to continue? yes/no");
+		String flag = scn.nextLine();
+
+		if (flag.equals("yes")) {
+			System.out.print("Choose an option again: ");
+			int option = scn.nextInt();
+			return option;
+		}
+		return 6;
+
+	}
+
+	// CASE 1
+	private static int checkSQLConnection() {
+
+		try {
+			// connect to database
+			Connection con = DriverManager.getConnection("jdbc:mysql://127.0.0.1:3306/database_name", "usename", "password");
+			System.out.println("\n" + con);
+			System.out.println("SQL connection successful!");
+
+			// close database connection
+			con.close();
+
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+
+		// check if user wants to continue or not
+		Scanner scn = new Scanner(System.in);
+		System.out.println("\nDo you want to continue? yes/no");
+		String flag = scn.nextLine();
+
+		if (flag.equals("yes")) {
+			System.out.print("Choose an option again: ");
+			int option = scn.nextInt();
+			return option;
+		}
+		return 6;
+
+	}
+
+}
